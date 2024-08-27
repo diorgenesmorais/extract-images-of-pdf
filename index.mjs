7cb84714721165b9eba0f18b654cb4280f1ecb52
Diorgenes Morais <diorgenesmorais@gmail.com>
2024-08-27 08:43:55 -0300

    feat: add process image (png and jpg)

--- index.mjs
+++ index.mjs
@@ -1,6 +1,29 @@
 import { getDocument, OPS } from 'pdfjs-dist/legacy/build/pdf.mjs';
 import fs from 'fs';
 import { PNG } from 'pngjs';
+import sharp from 'sharp';
+
+const processImage = async (img, pageNum, j) => {
+    if (img && img.data) {
+        const numChannels = img.data.length / (img.width * img.height);
+
+        if (numChannels === 4) { // could it be PNG
+            const png = new PNG({ width: img.width, height: img.height });
+            png.data = img.data;
+            const imgPath = `./images/image-${pageNum}-${j}.png`;
+            png.pack().pipe(fs.createWriteStream(imgPath));
+            console.log(`Image PNG: ${imgPath}`);
+        } else if (numChannels === 3) { // could it be JPEG
+            const imgPath = `./images/image-${pageNum}-${j}.jpg`;
+            await sharp(img.data, { raw: { width: img.width, height: img.height, channels: numChannels } })
+                .toFormat('jpeg')
+                .toFile(imgPath);
+            console.log(`Image JPEG: ${imgPath}`);
+        } else {
+            console.log(`Unknown image format on page ${pageNum}, index ${j}`);
+        }
+    }
+};
 
 const getImageBuffer = async (page, ops, pageNum) => {
     return new Promise(async (resolve, reject) => {
@@ -19,21 +42,7 @@ const getImageBuffer = async (page, ops, pageNum) => {
             }
 
             for (let j = 0; j < images.length; j++) {
-                const img = images[j];
-
-                if (img && img.width && img.height && img.data) {
-                    const png = new PNG({
-                        width: img.width,
-                        height: img.height
-                    });
-
-                    png.data = img.data;
-
-                    const imgPath = `./images/image-${pageNum}-${j}.png`;
-                    png.pack().pipe(fs.createWriteStream(imgPath));
-
-                    console.log(`Imagem salva em: ${imgPath}`);
-                }
+                await processImage(images[j], pageNum, j);
             }
 
             if (images.length === 0) {
@@ -61,6 +70,6 @@ const extractImagesFromPDF = async (pdfPath) => {
 const pdf1 = './meuPdf.pdf';
 const pdf2 = './Diorgenes_2024_pt.pdf';
 
-extractImagesFromPDF(pdf2)
+extractImagesFromPDF(pdf1)
     .then(() => console.log('Extração concluída'))
     .catch(err => console.error('Erro durante a extração:', err));
